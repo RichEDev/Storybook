4e01364a7a40cc68ea81dd268f721791da7ecb21
Richard Edwards <Richard.Edwards@selenity.com>
2017-11-22 16:24:17 +0000

    Update login response with employees feedback preferences

---
+++ ut_expenses/Schema Objects/Schemas/dbo/Tables/EmployeeMobileAppReviewPreferences.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[EmployeeMobileAppReviewPreferences]
+(
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[EmployeeId] [int] NOT NULL,
+	[NeverPromptForReview] [bit] NOT NULL, 
+    CONSTRAINT [PK_EmployeeMobileAppReviewPreferences] PRIMARY KEY ([Id]),
+)
+
