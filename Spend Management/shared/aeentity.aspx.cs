0e6edfd5954af97b11f249193dbd307cee20c53d
Duncan Parsons <duncanparsons@selenity.com>
2017-11-22 13:21:51 +0000

    Skip page load if not text/html
    Related Work Items: #146196

--- Spend Management/shared/aeentity.aspx.cs
+++ Spend Management/shared/aeentity.aspx.cs
@@ -94,7 +94,7 @@
             Master.UseDynamicCSS = true;
 
             //skips over load events if posted back from the HTML extender AjaxFileUploader
-            if (Request["start"] == null && Request["done"] == null && Request["complete"] == null)
+            if (Request["start"] == null && Request["done"] == null && Request["complete"] == null && Request.AcceptTypes[0] != "*/*")
             {
                 int formSelectionListValue = -1;
                 string formSelectionTextValue = null;
