7affa07662332d8d987b91f77a06e76a64505d7b
Suraj Bn <Suraj.Bn@taritas.com>
2017-11-29 13:12:40 +0530

    Bug fixes for international bank accounts

--- SpendManagementLibrary/Employees/Employee.cs
+++ SpendManagementLibrary/Employees/Employee.cs
@@ -1564,7 +1564,7 @@
                 databaseConnection.sqlexecute.Parameters.AddWithValue("@reference", this.BankAccountDetails.AccountReference);
                 databaseConnection.sqlexecute.Parameters.AddWithValue("@currencyid", this.Currency);
                 databaseConnection.sqlexecute.Parameters.AddWithValue("@countryid", this.BankAccountDetails.CountryId);
-                databaseConnection.sqlexecute.Parameters.AddWithValue("@SwiftCode", this.BankAccountDetails.SortCode);
+                databaseConnection.sqlexecute.Parameters.AddWithValue("@SwiftCode", this.BankAccountDetails.SwiftCode);
                 databaseConnection.sqlexecute.Parameters.AddWithValue("@Iban", this.BankAccountDetails.Iban);
 
                 if (this.AuthoriserLevelDetailId == null)
