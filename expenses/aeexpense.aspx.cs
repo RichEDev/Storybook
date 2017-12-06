6669546b43eac9e47e35502441fe3e14c908e787
Andrew Butlin <Andrew.Butlin@selenity.com>
2017-12-06 18:01:34 +0000

    Merged PR 195: Correct $g for non-existant field.
    Correct $g for non-existant field.
    Adjust mandatory icon so not visible unless fail
    
    Related Work Items: #146403
    
    Cherry picked from Pull request 194.
    
    Related work items: #146403

--- expenses/aeexpense.aspx.cs
+++ expenses/aeexpense.aspx.cs
@@ -1456,14 +1456,7 @@ public partial class aeexpense : System.Web.UI.Page
             row.Cells.Add(cell);
             cell = new TableCell();
             cell.ID = "exchangeratemandatory";
-            if (showExchangeRateTT == true)
-            {
-                cell.Text = "*";
-            }
-            else
-            {
-                cell.Text = string.Empty;
-            }            
+            cell.Text = string.Empty;           
             row.Cells.Add(cell);
             cell = new TableCell();
             cell.CssClass = "inputtd";
