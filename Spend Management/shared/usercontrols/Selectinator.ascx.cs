9d7dbcf0de275d0be4d5593764dff763fe175888
Ritsa Plikaditi <ritsa.plikaditi@software-europe.co.uk>
2018-01-24 15:56:46 +0000

    Fix for Error when editing greenlight record where form filter and parent filter have been applied on n:1 relationship
    Related Work Items: #149405

--- Spend Management/shared/usercontrols/Selectinator.ascx.cs
+++ Spend Management/shared/usercontrols/Selectinator.ascx.cs
@@ -270,11 +270,14 @@
         {
             if (this.Filters.Count > 0 && this.Filters.Exists(filter => filter.FilterOnEdit))
             {
+                var filterCount = this.Filters.Count;
+
                 foreach (var filter in this.Filters)
                 {
                     if (filter.FilterOnEdit)
                     {
-                        filter.Order = (byte) (this.Filters.Count - 1);
+                        filter.Order = (byte) (filterCount - 1);
+                        filterCount--;
                     }
                 }
             }
