29a35635c986a4f339e3721102e8fd1cd0d00d3a
Andrew Butlin <Andrew.Butlin@selenity.com>
2018-01-31 16:42:56 +0000

    Merged PR 522: Merge Features/TW_SSOSQL-Develop
    Updated the SPROC to remove the concatenated string query that was being overflowed.
    Now uses a temp table and pointer, no concatenation.
    
    Related work items: #149428, #149728

--- metabaseExpenses/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetAccountIdsBySingleSignOnIssuerUri.sql
+++ metabaseExpenses/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/GetAccountIdsBySingleSignOnIssuerUri.sql
@@ -2,24 +2,33 @@
 	@Hostname nvarchar(300),
 	@IssuerUri nvarchar(1000)
 AS
-    declare @sql nvarchar(4000) = '';
-	
-    select
-        @sql = @sql + 'select ''' + registeredusers.dbname + ''' as dbname, IssuerUri COLLATE DATABASE_DEFAULT as IssuerUri from ' + registeredusers.dbname + '.dbo.SingleSignOn union '
-    from
-        registeredusers
-			join accountsLicencedElements on accountsLicencedElements.accountID = registeredusers.accountid
-			join registeredUsersHostnames on registeredUsersHostnames.accountid = registeredusers.accountid
-				join hostnames on hostnames.hostnameID = registeredUsersHostnames.hostnameID
-    where
-        registeredusers.archived = 0
-        and accountsLicencedElements.elementID = 185 -- SingleSignOn
-        and hostnames.hostname = @Hostname
 
-	if LEN(@sql) > 0
-	begin
-		set @sql = 'select registeredusers.accountid from registeredusers join (' + SUBSTRING(@sql, 1, LEN(@sql) - 6) + ') sso on sso.dbname = registeredusers.dbname where sso.IssuerUri COLLATE DATABASE_DEFAULT = @IssuerUri'
-	    exec sp_executesql @sql, N'@IssuerUri nvarchar(1000)', @IssuerUri = @IssuerUri
-	end
+DECLARE @accounts IntPK;
+DECLARE @accountID int;
+DECLARE @dbname NVARCHAR(MAX);
+DECLARE @UriMatchQuery NVARCHAR(MAX);
+DECLARE @UriMatch int;
 
-RETURN 0
+DECLARE AccountCursor CURSOR FOR SELECT DISTINCT registeredusers.accountid, dbname FROM registeredusers 
+JOIN accountsLicencedElements on accountsLicencedElements.accountID = registeredusers.accountid
+JOIN registeredUsersHostnames on registeredUsersHostnames.accountid = registeredusers.accountid
+JOIN hostnames on hostnames.hostnameID = registeredUsersHostnames.hostnameID
+WHERE archived = 0 and accountsLicencedElements.elementID = 185 and hostnames.hostname = @hostname
+
+OPEN AccountCursor;
+FETCH NEXT FROM AccountCursor INTO
+@accountID, @dbname
+
+WHILE @@FETCH_STATUS = 0
+BEGIN
+set @UriMatchQuery = 'select @UriMatch = count(issueruri) from ' + @dbname + '.dbo.SingleSignOn where IssuerUri = ''' + @issueruri + ''''
+exec sp_executesql @UriMatchQuery, N'@UriMatch int out', @UriMatch out
+
+IF (@UriMatch > 0)
+	INSERT INTO @accounts VALUES (@accountId)
+FETCH NEXT FROM AccountCursor INTO @accountID, @dbname
+END
+CLOSE AccountCursor
+DEALLOCATE AccountCursor
+
+select * from @accounts
