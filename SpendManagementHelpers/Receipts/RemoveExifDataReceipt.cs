ae2f3a538a475a7afe276714e04b84540e7d4bed
Richard Edwards <Richard.Edwards@selenity.com>
2018-02-09 09:39:23 +0000

    fix mem stream issue

--- SpendManagementHelpers/Receipts/RemoveExifDataReceipt.cs
+++ SpendManagementHelpers/Receipts/RemoveExifDataReceipt.cs
@@ -21,9 +21,8 @@
         {
             using (inStream)
             {
-                using (outStream)
-                {
-                    Stream outStream = new MemoryStream();
+                using (Stream outStream = new MemoryStream())
+                {                  
                     byte[] jpegHeader = new byte[2];
                     jpegHeader[0] = (byte)inStream.ReadByte();
                     jpegHeader[1] = (byte)inStream.ReadByte();
