9f4e1e10fb825c34785d4425143c5edaba99bf93
Adam Bowler <Adam.Bowler@selenity.com>
2018-03-13 17:06:49 +0000

    Merged PR 779: Add tuning sql
    Add tuning sql
    
    Related work items: #150347, #151459

---
+++ ut_expenses/Schema Objects/Schemas/dbo/Tables/Indexes/idx_ProcessedReceipts_WalletReceiptId_DateTotalMerchant.sql
@@ -0,0 +1,5 @@
+﻿CREATE NONCLUSTERED INDEX [idx_ProcessedReceipts_WalletReceiptId_DateTotalMerchant] ON [dbo].[ProcessedReceipts] ([WalletReceiptId] ASC) INCLUDE (
+	[Date]
+	,[Total]
+	,[Merchant]
+	)
