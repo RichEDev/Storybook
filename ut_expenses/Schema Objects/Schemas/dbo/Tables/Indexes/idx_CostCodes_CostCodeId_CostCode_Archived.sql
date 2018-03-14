3971d7690baeb14810641fa29e41d57b40856d69
Richard Edwards <Richard.Edwards@selenity.com>
2018-03-14 12:31:22 +0000

    Merged PR 782: Add index for costcode table
    Add index for costcode table
    
    Related work items: #147358, #151631

---
+++ ut_expenses/Schema Objects/Schemas/dbo/Tables/Indexes/idx_CostCodes_CostCodeId_CostCode_Archived.sql
@@ -0,0 +1,6 @@
+﻿CREATE NONCLUSTERED INDEX [idx_CostCodes_CostCodeId_CostCode_Archived] ON [dbo].[costcodes]
+(
+	[costcode] ASC,
+	[costcodeid] ASC,
+	[archived] ASC
+)WITH (SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF) ON [PRIMARY]
