48e59afe45ae0c6b127884b37bb322d55b6c0296
Puneet Bhadauriya <Puneet.Bhadauriya@selenity.com>
2018-03-19 10:24:17 +0000

    Merged PR 796: Merge Merge/Mobile_Rename_Email_Template_To_Develop to develop
    Corrected the template for winphone (remove extra spaces)
    
    Related work items: #135499

--- SpendManagementLibrary/MobileDeviceNotifications/TemplateManager/PushNotificationTemplateWP.cs
+++ SpendManagementLibrary/MobileDeviceNotifications/TemplateManager/PushNotificationTemplateWP.cs
@@ -13,7 +13,7 @@
 		/// <returns>WP notification template</returns>
 		public static string GetNotificationMessage(string notificationMessage, string notificationParameter)
 		{
-			string template = $"<?xml version=\"1.0\" encoding=\"utf - 8\"?>< wp:Notification xmlns:wp = \"WPNotification\" >< wp:Toast >< wp:Text1 >Expenses</ wp:Text1 >< wp:Text2 >{notificationMessage}</ wp:Text2 >< wp:Param >?{notificationParameter}</ wp : Param ></ wp:Toast ></ wp:Notification > ";
+			string template = $"<?xml version=\"1.0\" encoding=\"utf-8\"?><wp:Notification xmlns:wp = \"WPNotification\"><wp:Toast><wp:Text1>Expenses</wp:Text1><wp:Text2>{notificationMessage}</wp:Text2><wp:Param>?{notificationParameter}</wp:Param></wp:Toast></wp:Notification> ";
 			return template;
 		}
 	}
