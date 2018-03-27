baac09e1904c2bd4bea85732a2f3dece2389b290
Ritsa Plikaditi <ritsa.plikaditi@selenity.com>
2018-03-27 14:27:18 +0100

    Merged PR 837: Add SavedExpensesFuelReceiptToMileageAllocations in ut_expenses
    Add SavedExpensesFuelReceiptToMileageAllocations in ut_expenses
    
    Related work items: #152044

--- ut_expenses/Schema Objects/Schemas/dbo/Tables/Keys/FK_SavedExpensesFuelReceiptToMileageAllocations_savedexpenses_mileageexpenseid.sql
+++ ut_expenses/Schema Objects/Schemas/dbo/Tables/Keys/FK_SavedExpensesFuelReceiptToMileageAllocations_savedexpenses_mileageexpenseid.sql
@@ -1,1 +1,3 @@
-﻿ALTER TABLE [dbo].[SavedExpensesFuelReceiptToMileageAllocations] CHECK CONSTRAINT [FK_SavedExpensesFuelReceiptToMileageAllocations_savedexpenses_mileageexpenseid]
+﻿ALTER TABLE [dbo].[SavedExpensesFuelReceiptToMileageAllocations]  WITH CHECK ADD  CONSTRAINT [FK_SavedExpensesFuelReceiptToMileageAllocations_savedexpenses_mileageexpenseid] FOREIGN KEY([MileageExpenseId])
+REFERENCES [dbo].[savedexpenses] ([expenseid])
+ON DELETE CASCADE
---
+++ ut_expenses/Schema Objects/Schemas/dbo/Tables/SavedExpensesFuelReceiptToMileageAllocations.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[SavedExpensesFuelReceiptToMileageAllocations](
+       [FuelReceiptToMileageAllocationId] [int] IDENTITY(1,1) NOT NULL,
+       [MileageExpenseId] [int] NOT NULL,
+       [FuelReceiptExpenseId] [int] NOT NULL,
+       [FuelReceiptVatAmountAllocated] [money] NOT NULL,
+       [CreatedOn] [datetime] NOT NULL);
