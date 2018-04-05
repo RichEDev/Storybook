e928b0edd157829ae8da58f69a579140ac850f91
Andrew Butlin <Andrew.Butlin@selenity.com>
2018-04-05 09:42:12 +0100

    Merged PR 872: Additional CASE for null vs relatedtable/keyfield for AS lookuptable/lookupfield
    Additional CASE for null vs relatedtable/keyfield for AS lookuptable/lookupfield
    
    Related work items: #152163, #152253

--- ut_expenses/Schema Objects/Schemas/dbo/Views/CustomerFields.sql
+++ ut_expenses/Schema Objects/Schemas/dbo/Views/CustomerFields.sql
@@ -3,30 +3,30 @@ AS
 SELECT
   dbo.[customentityattributes].fieldid,
   CASE 
-    WHEN dbo.[customEntityAttributes].fieldtype = 9 AND customEntityAttributes.display_name <> 'Created On' AND 
-                      customEntityAttributes.display_name <> 'Created By' AND customEntityAttributes.display_name <> 'Modified On' AND 
-                      customEntityAttributes.display_name <> 'Modified By' AND  customEntityAttributes.display_name  <> 'Archived' THEN 
+    WHEN dbo.[customEntityAttributes].fieldtype = 9 AND dbo.[customEntityAttributes].relationshiptype = 2 AND dbo.[customEntityAttributes].display_name <> 'Created On' AND 
+                      dbo.[customEntityAttributes].display_name <> 'Created By' AND dbo.[customEntityAttributes].display_name <> 'Modified On' AND 
+                      dbo.[customEntityAttributes].display_name <> 'Modified By' AND  dbo.[customEntityAttributes].display_name  <> 'Archived' THEN 
 					  dbo.[customEntityAttributes].relatedtable
     ELSE
   dbo.[customentities].tableid
   END AS tableid,
-  CASE dbo.[customentityattributes].display_name
+  CASE dbo.[customEntityAttributes].display_name
     WHEN 'GreenLight Currency' THEN 'GreenLightCurrency'
-    WHEN 'Created On' THEN REPLACE([customentityattributes].display_name, ' ', '')
-    WHEN 'Created By' THEN REPLACE([customentityattributes].display_name, ' ', '')
-    WHEN 'Modified On' THEN REPLACE([customentityattributes].display_name, ' ', '')
-    WHEN 'Modified By' THEN REPLACE([customentityattributes].display_name, ' ', '')
-	WHEN 'Archived' THEN CASE dbo.[customentityattributes].fieldtype WHEN 5 THEN 'Archived' ELSE 'att' + CAST(dbo.[customentityattributes].attributeid AS nvarchar(10)) END
-    ELSE 'att' + CAST(dbo.[customentityattributes].attributeid AS nvarchar(10))
+    WHEN 'Created On' THEN REPLACE(dbo.[customEntityAttributes].display_name, ' ', '')
+    WHEN 'Created By' THEN REPLACE(dbo.[customEntityAttributes].display_name, ' ', '')
+    WHEN 'Modified On' THEN REPLACE(dbo.[customEntityAttributes].display_name, ' ', '')
+    WHEN 'Modified By' THEN REPLACE(dbo.[customEntityAttributes].display_name, ' ', '')
+	WHEN 'Archived' THEN CASE dbo.[customEntityAttributes].fieldtype WHEN 5 THEN 'Archived' ELSE 'att' + CAST(dbo.[customentityattributes].attributeid AS nvarchar(10)) END
+    ELSE 'att' + CAST(dbo.[customEntityAttributes].attributeid AS nvarchar(10))
   END AS field,
   CASE dbo.[customEntityAttributes].fieldtype
     WHEN '17' THEN N'CL'	
     ELSE dbo.getFieldType(dbo.[customEntityAttributes].fieldtype, dbo.[customEntityAttributes].format)
   END AS fieldtype,
-  dbo.[customentityattributes].display_name AS description,
-  dbo.[customentityattributes].display_name AS comment,
+  dbo.[customEntityAttributes].display_name AS description,
+  dbo.[customEntityAttributes].display_name AS comment,
   CAST(0 AS bit) AS normalview,
-  dbo.[customentityattributes].is_key_field AS idfield,
+  dbo.[customEntityAttributes].is_key_field AS idfield,
   dbo.[customentities].tableid AS viewgroupid,
   CAST(0 AS BIT) AS genlist,
   CAST(50 AS INT) AS width,
@@ -37,10 +37,28 @@ SELECT
     ELSE CAST(0 AS BIT)
   END AS valuelist,
   CAST(1 AS BIT) AS allowimport,
-  dbo.[customentityattributes].mandatory,
-  dbo.[customentityattributes].modifiedon AS amendedon,
-  dbo.[customentityattributes].relatedtable AS lookuptable,
-  dbo.[tables].keyfield AS lookupfield,
+  dbo.[customEntityAttributes].mandatory,
+  dbo.[customEntityAttributes].modifiedon AS amendedon,
+  CASE 
+	WHEN ((dbo.[customEntityAttributes].fieldtype = 9 AND dbo.[customEntityAttributes].relationshiptype = 2 AND dbo.[customEntityAttributes].display_name <> 'Created On' AND 
+                      dbo.[customEntityAttributes].display_name <> 'Created By' AND dbo.[customEntityAttributes].display_name <> 'Modified On' AND 
+                      dbo.[customEntityAttributes].display_name <> 'Modified By' AND  dbo.[customEntityAttributes].display_name  <> 'Archived') 
+					  OR (dbo.[customEntityAttributes].fieldtype = 17 AND dbo.[customEntityAttributes].display_name = 'GreenLight Currency')
+					  OR (dbo.[customEntityAttributes].fieldtype = 5 AND dbo.[customEntityAttributes].display_name = 'Archived')
+					  OR (dbo.[customEntityAttributes].fieldtype = 22 AND dbo.[customEntityAttributes].display_name = 'Attachment'))
+	THEN null
+	ELSE dbo.[customEntityAttributes].relatedtable
+  END AS lookuptable,
+  CASE 
+	WHEN ((dbo.[customEntityAttributes].fieldtype = 9 AND dbo.[customEntityAttributes].relationshiptype = 2 AND dbo.[customEntityAttributes].display_name <> 'Created On' AND 
+                      dbo.[customEntityAttributes].display_name <> 'Created By' AND dbo.[customEntityAttributes].display_name <> 'Modified On' AND 
+                      dbo.[customEntityAttributes].display_name <> 'Modified By' AND  dbo.[customEntityAttributes].display_name  <> 'Archived') 
+					  OR (dbo.[customEntityAttributes].fieldtype = 17 AND dbo.[customEntityAttributes].display_name = 'GreenLight Currency')
+					  OR (dbo.[customEntityAttributes].fieldtype = 5 AND dbo.[customEntityAttributes].display_name = 'Archived')
+					  OR (dbo.[customEntityAttributes].fieldtype = 22 AND dbo.[customEntityAttributes].display_name = 'Attachment'))
+	THEN null
+	ELSE dbo.[tables].keyfield
+  END AS lookupfield,
   CAST(0 AS BIT) AS useforlookup,
   CAST(1 AS BIT) AS workflowUpdate,
   CAST(1 AS BIT) AS workflowSearch,
@@ -51,10 +69,10 @@ SELECT
   CAST(0 AS BIT) AS allowDuplicateChecking,
   NULL AS classPropertyName,
   CAST(0 AS TINYINT) AS FieldCategory,
-  customEntityAttributes.relatedtable AS RelatedTable,
-  CASE (dbo.[customentityattributes].fieldtype)
+  dbo.[customEntityAttributes].relatedtable AS RelatedTable,
+  CASE (dbo.[customEntityAttributes].fieldtype)
     WHEN '22' THEN CAST(1 AS BIT)
-    WHEN '9' THEN CASE dbo.[customentityattributes].relationshiptype
+    WHEN '9' THEN CASE dbo.[customEntityAttributes].relationshiptype
         WHEN 1 THEN CAST(1 AS BIT)
         ELSE CAST(0 AS BIT)
       END
@@ -64,11 +82,11 @@ SELECT
   NULL AS DuplicateCheckingSource,
   NULL AS DuplicateCheckingCalculation,
   [Encrypted]
-FROM dbo.[customentityattributes]
+FROM dbo.[customEntityAttributes]
 INNER JOIN dbo.[customentities]
-  ON dbo.[customentities].entityid = dbo.[customentityattributes].entityid
+  ON dbo.[customentities].entityid = dbo.[customEntityAttributes].entityid
 LEFT JOIN tables
-  ON tables.tableid = dbo.[customentityattributes].relatedtable
+  ON tables.tableid = dbo.[customEntityAttributes].relatedtable
 UNION
 SELECT
   fieldid,
