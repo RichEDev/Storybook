9d41c761c6c3fdc24f7b0126a77dcab11970067d
Richard Edwards <Richard.Edwards@selenity.com>
2018-05-30 11:08:43 +0100

    Merged PR 1143: Merge Merge/Int_AddClaimApproveDetailsIndices_ToDev to develop
    Related work items: #153046, #153084, #153364

---
+++ ut_expenses/Schema Objects/Schemas/dbo/Tables/Indexes/IX_ClaimApproverDetails_CheckerId_ClaimId_SavedExpenseId_ClaimApproverId.sql
@@ -0,0 +1,10 @@
+﻿CREATE NONCLUSTERED INDEX [IX_ClaimApproverDetails_CheckerId_ClaimId_SavedExpenseId_ClaimApproverId] ON [dbo].[ClaimApproverDetails]
+(
+	[CheckerId] ASC,
+	[ClaimId] ASC,
+	[SavedExpenseId] ASC,
+	[ClaimApproverDetailId] ASC
+)
+INCLUDE ( 	[ClaimantId],
+	[CreatedOn],
+	[ClaimAmount]) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
---
+++ ut_expenses/Schema Objects/Schemas/dbo/Tables/Indexes/IX_ClaimApproverDetails_ClaimantId_CheckerId_CreatedOn.sql
@@ -0,0 +1,7 @@
+﻿CREATE NONCLUSTERED INDEX IX_ClaimApproverDetails_ClaimantId_CheckerId_CreatedOn ON [dbo].[ClaimApproverDetails]
+(
+	[ClaimantId] ASC,
+	[CheckerId] ASC,
+	[CreatedOn] ASC
+)
+INCLUDE ([ClaimAmount]) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
